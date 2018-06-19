ba0e8f13ec516341fab644998ae7a8b56e8d99fc
Marek Korcz <mark.korcz@gmail.com>
2018-06-19 07:17:00 +0200

    Configuring connection with DataBase for tests. Creating login and register tests.

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -13,7 +13,7 @@ class SecurityControllerTest extends WebTestCase
         $this->client = static::createClient();
     }
     
-    public function testCustomerRegisterPageShow()
+    public function testUserRegisterPageShow()
     {
         $crawler = $this->client->request('GET', '/register');
 
@@ -21,11 +21,46 @@ class SecurityControllerTest extends WebTestCase
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Register Page")')->count());
     }
     
-    public function testCustomerLoginPageShow()
+    public function testUserLoginPageShow()
     {
         $crawler = $this->client->request('GET', '/login');
 
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Login Page")')->count());
+    }
+    
+    public function testUserLoginAction()
+    {
+        $crawler = $this->client->request('GET', '/login');    
+        
+        $form = $crawler->selectButton('login')->form([
+            '_username' => 'mariano.italiano@gmail.com',
+            '_password' => 'lalalalala'
+        ]);
+        
+        $this->client->submit($form);
+        
+        $this->client->followRedirect();
+        
+        $this->assertContains('Homepage', $this->client->getResponse()->getContent());
+    }
+    
+    public function testUserRegistration()
+    {
+        $crawler = $this->client->request('GET', '/register');
+
+        $form = $crawler->selectButton('Submit')->form([
+            'user[name]'                => 'Dawid',
+            'user[surname]'             => 'Podsiadlo',
+            'user[email]'               => 'dawid.podsiadlo@gmail.com',
+            'user[phoneNumber]'         => '113653623',
+            'user[password][first]'     => 'lalalalala',
+            'user[password][second]'    => 'lalalalala'
+        ]);
+
+        $this->client->submit($form);
+        
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertContains('Login Page', $this->client->getResponse()->getContent());
     }
 }
