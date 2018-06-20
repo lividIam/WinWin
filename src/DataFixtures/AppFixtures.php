f3b20af2a08484f45bc8b13c8a602f875dc0e377
Marek Korcz <mark.korcz@gmail.com>
2018-06-20 06:52:24 +0200

    Adding fixture for Admin

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
+use App\Entity\Admin;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -18,15 +19,27 @@ class AppFixtures extends Fixture
     
     public function load(ObjectManager $manager) {
         
+        // load User section
         $user = new User();
         $user->setName('Mariano');
         $user->setSurname('Italiano');
         $user->setEmail('mariano.italiano@gmail.com');
         $user->setPhoneNumber('111222333');
-        $password = $this->encoder->encodePassword($user, 'lalalalala');
-        $user->setPassword($password);
-
+        $userPassword = $this->encoder->encodePassword($user, 'lalalalala');
+        $user->setPassword($userPassword);
+        
         $manager->persist($user);
+        
+        
+        // load Admin section
+        $admin = new Admin();
+        $admin->setEmail('mark.korcz@gmail.com');
+        $adminPassword = $this->encoder->encodePassword($admin, 'lalalalala');
+        $admin->setPassword($adminPassword);
+        
+        $manager->persist($admin);
+        
+        
         $manager->flush();
     }
 }
