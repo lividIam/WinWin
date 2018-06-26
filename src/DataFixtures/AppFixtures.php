d3ba2c703625c5a72815b2f2552d1fde44db4f12
Marek Korcz <mark.korcz@gmail.com>
2018-06-26 07:18:59 +0200

    Adding fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,15 @@ namespace App\DataFixtures;
 
 use App\Entity\Person\User;
 use App\Entity\Person\Admin;
+use App\Entity\Store\Store;
+use App\Entity\Product\Product;
+use App\Entity\Product\Category;
+use App\Entity\Product\Product_Details;
+use App\Entity\Product\Details\Manufacturer;
+use App\Entity\Product\Details\Model;
+use App\Entity\Product\Details\Version;
+use App\Entity\Product\Details\Color;
+use App\Entity\Product\Details\Shape;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -19,7 +28,7 @@ class AppFixtures extends Fixture
     
     public function load(ObjectManager $manager) {
         
-        // load User section
+        // load User
         $user = new User();
         $user->setName('Mariano');
         $user->setSurname('Italiano');
@@ -30,13 +39,89 @@ class AppFixtures extends Fixture
         $manager->persist($user);
         
         
-        // load Admin section
+        // load Admin
         $admin = new Admin();
         $admin->setEmail('mark.korcz@gmail.com');
         $adminPassword = $this->encoder->encodePassword($admin, 'lalalalala');
         $admin->setPassword($adminPassword);
         
         $manager->persist($admin);
+        
+        
+        // load Store
+        $store = new Store();
+        $store->setName('Tech Land');
+        // need logic for adding picture
+        $store->setLogo('logo.png');
+        $store->setOwner($user);
+        
+        $manager->persist($store);
+        
+        
+        // load Manufacturer detail
+        $manufacturer = new Manufacturer();
+        $manufacturer->setName('Apple');
+        
+        $manager->persist($manufacturer);
+        
+        
+        // load Model detail
+        $model = new Model();
+        $model->setName('iPhone');
+        
+        $manager->persist($model);
+        
+        
+        // load Version detail
+        $version = new Version();
+        $version->setName('6s');
+        
+        $manager->persist($version);
+        
+        
+        // load Color detail
+        $color = new Color();
+        $color->setName('red');
+        
+        $manager->persist($color);
+        
+        
+        // load Shape detail
+        $shape = new Shape();
+        $shape->setName('square...');
+        
+        $manager->persist($shape);
+        
+        
+        // load Product_Details
+        $productDetail = new Product_Details();
+        $productDetail->setManufacturer($manufacturer);
+        $productDetail->setModel($model);
+        $productDetail->setVersion($version);
+        $productDetail->setColor($color);
+        $productDetail->setShape($shape);
+        $productDetail->setQuantity(12);
+        $productDetail->setPrice(2000);
+        
+        $manager->persist($productDetail);
+        
+        
+        // load Category
+        $category = new Category();
+        $category->setName('Mobile Phone');
+        
+        // load Product
+        $product = new Product();
+        $product->setName('iPhone 6s');
+        $product->setDescription('New Apple iPhone');
+        $product->setProductDetail($productDetail);
+        $product->setCategory($category);
+        $product->setStore($store);
+        
+        $category->setProduct($product);
+        $manager->persist($category);
+        
+        $manager->persist($product);
         
         
         $manager->flush();
