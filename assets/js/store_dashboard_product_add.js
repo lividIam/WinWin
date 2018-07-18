b7a61941504b2dd71ffddc004698931d96cb8a30
Marek Korcz <mark.korcz@gmail.com>
2018-07-18 07:16:49 +0200

    Adding most of accordions working mechanism (still with dummy data and without subcategories summary on top and passing chosen category to back form)

--- assets/js/store_dashboard_product_add.js
+++ assets/js/store_dashboard_product_add.js
@@ -1,83 +1,201 @@
 $(document).ready(function() {
     
+    // dummpy data (ajax responses in a future)
+    var params = ["One", "Two", "Three"];
+    
+    // if someone clicks on "Choose category" button
+    $("#product_category").click(function() {
+        
+        // show wrapper
+        $('#popup-wrapper').css('visibility', 'visible');
+        
+        
+        
+        
+        // add div on top of accordion's cards with summary of currently chosen subcategories !!!!!
+        
+        
+        
+        
+        // add first element to it
+        var element = pasteAccordionCardElement(1, params);
+        $("#accordion").append(element);
+        
+        // open the element
+        $("button[data-target='#collapse_1']").removeClass('collapsed').attr('aria-expanded', 'true');
+        $("#collapse_1").addClass('show');
+    });
+    
     $(window).click(function(event) {
+        
+        // if someone clicks beyond popup-content
         if (event.target === document.getElementById('popup-wrapper')) {
+            
+            // hide wrapper
             $('#popup-wrapper').css('visibility', 'hidden');
+            
+            // delete all elements in accordion
+            $("#accordion").empty();
         }
     });
     
-    $("#product_category").click(function() {
-        $('#popup-wrapper').css('visibility', 'visible');
-    });
     
 //    $("#close").click(function() {
 //        $('#popup-wrapper').css('visibility', 'hidden');
 //    });
 
-    $("#collapse1 li").click(function(event) {
-        
-        handleFirstCollapsiblePanel(event);
-        
-//        resetAllFollowingSiblings();
-        
-        preHandleSecondCollapsiblePanel();
+
+    // if someone clicks on one of the first list, items
+    $(document).on('click', "#collapse_1 li", function(event) {
+                
+        handleAccordionCardClickEvent(1, event, params);
     });
     
-    $("#collapse2 li").click(function(event) {
+    // if someone clicks on one of the second list, items
+    $(document).on('click', "#collapse_2 li", function(event) {
         
-        // after click close first panel if its open
-        
-        
-        handleSecondCollapsiblePanel(event);
+        handleAccordionCardClickEvent(2, event, params);
     });
     
-    function handleFirstCollapsiblePanel(event){
+    // if someone clicks on one of the third list, items
+    $(document).on('click', "#collapse_3 li", function(event) {
         
-        $("#collapse1 li").each(function() {
+        // if color is already set, disable it
+        $("#collapse_3 li").each(function() {
             if($(this).attr('data-checked') === 'true') {
                 $(this).css('background-color', '').removeAttr('data-checked');
             }
         });
         
-        $(event.target).css('background-color', 'aqua').attr('data-checked', 'true');
-        $(".panel-heading a[href='#collapse1']").html(event.target.innerHTML);
+        changeListTailColorAndHeadTailTitle(3, event);
         
-        // remove all the following siblings
-    }
-    
-//    function resetAllFollowingSiblings() {
-//        
-//        $("div#first").nextAll().each(function(index) {
-//            $(".panel-heading a[href='#collapse" + index + "']").html("Choose category");
-//            
-//            if($(this).attr('data-checked') === 'true') {
-//                $(this).css('background-color', '').removeAttr('data-checked');
-//            }
-//            
-//            if($(this).css('visibility') === 'visible') {
-//                $(this).css('visibility', 'hidden');
-//            }
-//        });
-//    }
-    
-    function preHandleSecondCollapsiblePanel() {
+        $("button[data-target='#collapse_3']").addClass('collapsed').attr('aria-expanded', 'false');
+        $("#collapse_3").removeClass('show');
         
-        $("#collapse1").removeClass("show");
-        $("#second").css('visibility', 'visible');
-        $("#collapse2 li").each(function() {
+        // add submit button
+        var button = pasteSubmitButton();
+        $("#accordion").append(button);
+    });
+    
+    // if someone clicks on the submit button appended after choosing category
+    $(document).on('click', "#submit_chosen_category", function() {
+        
+        
+        
+        
+        // move chosen data to form and display it there !!!!!
+        
+        
+        
+        
+        // hide wrapper
+        $('#popup-wrapper').css('visibility', 'hidden');
+
+        // delete all elements in accordion
+        $("#accordion").empty();
+    });
+    
+    // handle appending next accordion card, changing list tail color, card opening and closing
+    function handleAccordionCardClickEvent(index, event, params) {
+        
+        var element = pasteAccordionCardElement(index + 1, params);
+        
+        // if color is already set, disable it and remove all next child elements
+        $("#collapse_" + index + " li").each(function() {
             if($(this).attr('data-checked') === 'true') {
+                
                 $(this).css('background-color', '').removeAttr('data-checked');
+                
+                $("#accordion > div:nth-child(" + index + ")").nextAll().remove();
             }
         });
+        
+        changeListTailColorAndHeadTailTitle(index, event);
+        
+        // close current card element
+        $("button[data-target='#collapse_" + index + "']").addClass('collapsed').attr('aria-expanded', 'false');
+        $("#collapse_" + index + "").removeClass('show');
+        
+        $("#accordion").append(element);
+        
+        // open next card element
+        $("button[data-target='#collapse_" + (index + 1) + "']").removeClass('collapsed').attr('aria-expanded', 'true');
+        $("#collapse_" + (index + 1) + "").addClass('show');
     }
     
-    function handleSecondCollapsiblePanel(event) {
+    // changing list tail color and head tail title
+    function changeListTailColorAndHeadTailTitle(index, event) {
         
-        preHandleSecondCollapsiblePanel();
-        
-        $("#collapse1").removeClass("show");
         $(event.target).css('background-color', 'aqua').attr('data-checked', 'true');
-        $(".panel-heading a[href='#collapse2']").html(event.target.innerHTML);
-        $("#collapse2").removeClass("show");
+        $("div#heading_" + index + " button[data-target='#collapse_" + index + "']").html(event.target.innerHTML);
+    }
+    
+    // creating and pasting new accordion card element
+    function pasteAccordionCardElement(value, params) {
+        
+        var paramsLength = params.length;
+        
+        // main div
+        var div_1 = document.createElement('div');
+        div_1.setAttribute('class', 'card');
+        
+        // first of two insides divs
+        var div_2 = document.createElement('div');
+        div_2.setAttribute('class', 'card-header');
+        div_2.setAttribute('id', 'heading_' + value);
+        
+        var h5 = document.createElement('h5');
+        h5.setAttribute('class', 'mb-0');
+        
+        var button = document.createElement('button');
+        button.setAttribute('class', 'btn btn-link');
+        button.setAttribute('data-toggle', 'collapse');
+        button.setAttribute('data-target', '#collapse_' + value);
+        button.setAttribute('aria-expanded', 'true');
+        button.setAttribute('aria-controls', 'collapse_' + value);
+        button.innerHTML = "Choose category";
+        
+        h5.appendChild(button);
+        div_2.appendChild(h5);
+        
+        // second of two insides divs
+        var div_3 = document.createElement('div');
+        div_3.setAttribute('id', 'collapse_' + value);
+        div_3.setAttribute('class', 'collapse');
+        div_3.setAttribute('aria-labelledby', 'heading_' + value);
+        div_3.setAttribute('data-parent', '#accordion');
+        
+        var div_4 = document.createElement('div');
+        div_4.setAttribute('class', 'card-body');
+        
+        var ul = document.createElement('ul');
+        ul.setAttribute('class', 'list-group');
+        
+        for(var i = 0; i < paramsLength; i++) {
+            var li = document.createElement('li');
+            li.setAttribute('class', 'list-group-item');
+            li.innerHTML = params[i];
+            ul.appendChild(li);
+        }
+        
+        div_4.appendChild(ul);
+        div_3.appendChild(div_4);
+        
+        // append inside divs to main div
+        div_1.appendChild(div_2);
+        div_1.appendChild(div_3);
+        
+        return div_1;
+    }
+    
+    // creating and pasting submit button
+    function pasteSubmitButton() {
+        
+        var button = document.createElement('button');
+        button.setAttribute('id', 'submit_chosen_category');
+        button.setAttribute('type', 'button');
+        button.innerHTML = "Submit";
+        
+        return button;
     }
 });
